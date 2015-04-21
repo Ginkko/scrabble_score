6b80abb840909f294c09c016c2b4128954131a2a
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 11:26:15 -0700

    Pass initial spec

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,5 +1,20 @@
 class String
-  define_method(:scrablle_score) do
+  define_method(:scrabble_score) do
+
+    score1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+    score2 = ["D", "G"]
+    score3 = ["B", "C", "M", "P"]
+    score4 = ["F", "H", "V", "W", "Y"]
+    score5 = ["K"]
+    score8 = ["J", "X"]
+    score10 = ["Q", "Z"]
+
+    if score1.include?(self)
+      return 1
+    elsif score2.include?(self)
+      return 2
+    end
+
     #Returns a predefined numerical score for each letter
 
     #Break down each string into component letters
@@ -7,3 +22,4 @@ class String
     #Add total score together from component letters.
 
   end
+end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,6 +3,11 @@ require('scrabble_score')
 require('pry')
 
 #Returns a predefined numerical score for each letter
+describe('String#scrablle_score') do
+  it("returns a scrabble score for a letter") do
+    expect("A".scrabble_score()).to(eq(1))
+  end
+end
 
 #Break down each string into component letters
 
