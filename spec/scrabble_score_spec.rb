8f266fddb948de44ce18067fe8db8e6037e3985b
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 11:35:30 -0700

    Add second spec

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -7,8 +7,12 @@ describe('String#scrablle_score') do
   it("returns a scrabble score for a letter") do
     expect("A".scrabble_score()).to(eq(1))
   end
+  #Break down each string into component letters
+  it("breaks down the input into an array of letters") do
+    expect("Gym".scrabble_score()).to(eq(["G", "y", "m"]))
+  end
 end
 
-#Break down each string into component letters
+
 
 #Add total score together from component letters.
