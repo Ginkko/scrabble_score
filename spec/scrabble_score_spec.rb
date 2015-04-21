5e41a15a31a76d8f504c296915a3c0101ea69bc9
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 10:57:08 -0700

    Create inital files and plain text specs

---
+++ spec/scrabble_score_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('scrabble_score')
+require('pry')
+
+#Returns a predefined numerical score for each letter
+
+#Break down each string into component letters
+
+#Add total score together from component letters.
