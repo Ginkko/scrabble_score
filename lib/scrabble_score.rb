5f6600ff13e412b194ef71493f61a9fade669c67
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 10:59:28 -0700

    Creare method shell

---
+++ lib/scrabble_score.rb
@@ -0,0 +1,9 @@
+class String
+  define_method(:scrablle_score) do
+    #Returns a predefined numerical score for each letter
+
+    #Break down each string into component letters
+
+    #Add total score together from component letters.
+
+  end
